50d05264bffa7c6fb11deaae055cfefb32f535d6
face0u0 <me@face0u0.net>
2021-01-18 21:25:15 +0900

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
             {
                 test: /\.(jpg|png|svg)$/,
                 use: 'file-loader'
-            }   ,
+            },
         ]
     },
     plugins: [
