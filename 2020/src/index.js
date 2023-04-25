1394a6f841fad3f9c736ef35d34258f13420efff
mu2chn <app@mu2chn.net>
2023-04-25 14:07:24 +0900

    push

--- 2020/src/index.js
+++
@@ -1,3 +0,0 @@
-import './js/pages/index'
-
-
--- 2020/src/js/elements/entry.js
+++
@@ -1,5 +0,0 @@
-import './templates/serviceSection'
-import './layouts/header'
-import './layouts/footer'
-import './layouts/hero'
-import  './templates/defaultSection'
